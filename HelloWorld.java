0e50783179fd8604ff37f43ce990feef95f21b49
Колесников Семен Борисович <s.kolesnikov@univ.priv>
2021-09-16 15:44:12 +0300

    cherry pick form new-config

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main (String args[]){
-	System.out.println("Hello, World!1");
+	System.out.println("Hello, World!\n");
 	System.out.println("New Feature!");
     }
 }
